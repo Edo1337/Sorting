7c31cb8d922b9049fd8af5b14d047fc353e4cca2
Эдуард Гладков <eduard.gladkov.00@gmail.com>
2023-11-11 23:58:44 +0300

    Добавьте файлы проекта.

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    static class ArrayPlus
+    {
+        public static void Show(int[] array)
+        {
+            foreach (int i in array)
+                Console.WriteLine(i);
+        }
+
+        /// <summary>
+        /// Создает рандомный массив указанной длины
+        /// </summary>
+        /// <paramk name="arrayLenght"></param>
+        /// <returns>Рандомный массив</returns>
+        public static int[] ArrRnd(int arrayLenght)
+        {
+            int[] arr = new int[arrayLenght];
+            Random random = new Random();
+
+            for (int i = 0; i < arrayLenght - 1; i++)
+                arr[i] = random.Next(0, 1000);
+
+            return arr;
+        }
+    }
+    static class BinarySearch
+    {
+        private static void ShowInfo(ref int searchedElement, ref int index, ref uint i)
+        {
+            Console.WriteLine($"Элемент массива {searchedElement} под индексом {index} найден за {i} циклов");
+        }
+        public static int Find(ref int[] array, ref int searchedElement)
+        {
+            var left = 0;
+            var right = array.Length - 1;
+
+            uint i = default;
+
+            while (left <= right)
+            {
+                var middle = (left + right) / 2;
+                if (searchedElement > array[middle])
+                    left = middle + 1;
+                else if (searchedElement < array[middle])
+                    right = middle - 1;
+                else
+                {
+                    ShowInfo(ref searchedElement, ref middle, ref i);
+                    return middle;
+                }
+                i++;
+            }
+            return -1;
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = ArrayPlus.ArrRnd(100);
+
+            Array.Sort(arr);
+
+            ArrayPlus.Show(arr);
+
+            Console.Write($"Какой элемент массива вы хотите найти: ");
+            int elementForFind = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(BinarySearch.Find(ref arr, ref elementForFind));
+        }
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        private static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        public static int[] BubbleSort(ref int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        Swap(ref arr[j], ref arr[j + 1]);
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] nums = new int[34] { 214, 532, 124, 531, 312, 53, 7, 18, 7, 57, 5, 48, 86, 68, 68, 3, 11, 63, 5, 7, 56, 3, 6, 6, 89, 9, 34, 5, 8, 7, 3, 45, 3, 76 };
+
+            BubbleSort(ref nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
